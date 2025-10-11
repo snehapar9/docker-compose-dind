c1da0110d51bc4ca1c011075e21194de0f8376bf
Sneha Parthasarathy <snehapar@microsoft.com>
2025-10-10 17:01:18 -0700

    Remount /proc/sys

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+# Remount /proc/sys as read-write
+echo "Remounting /proc/sys as read-write..."
+mount -o remount,rw /proc/sys
+
 # Enable and start the Docker daemon
 echo "Enabling and starting dockerd..."
 /usr/bin/dockerd --ipv6=true --ip6tables=true --fixed-cidr-v6=fd00:dead:beef::/48 --rootless > /var/log/dockerd.log 2>&1 &
